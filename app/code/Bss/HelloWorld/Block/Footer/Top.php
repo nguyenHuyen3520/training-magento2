820e7e9944a38682f912db9057d14c2b89d491c5
Dev-Huyên <61755848+nguyenHuyen3520@users.noreply.github.com>
2024-01-17 18:25:29 +0700

    week4-Day 2

--- app/code/Bss/HelloWorld/Block/Footer/Top.php
+++ app/code/Bss/HelloWorld/Block/Footer/Top.php
@@ -8,40 +8,7 @@
 namespace Bss\HelloWorld\Block\Footer;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Cms\Model\BlockFactory;
 
 class Top extends Template
 {
-    /**
-     * @var \Magento\Cms\Model\BlockFactory
-     */
-    protected $blockFactory;
-
-    /**
-     * CustomBlock constructor.
-     *
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Cms\Model\BlockFactory $blockFactory
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        BlockFactory $blockFactory,
-        array $data = []
-    ) {
-        parent::__construct($context, $data);
-        $this->blockFactory = $blockFactory;
-    }
-
-    /**
-     * Get the content of the static block by identifier
-     *
-     * @param string $blockIdentifier
-     * @return string
-     */
-    public function getStaticBlockContent($blockIdentifier)
-    {
-        $block = $this->blockFactory->create()->load($blockIdentifier);
-        return $block->getContent();
-    }
 }
